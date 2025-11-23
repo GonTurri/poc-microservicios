770f8ce1974606343be2ece8d5cf2512a7381493
Gonzalo Ivo Turri <gturri@frba.utn.edu.ar>
2025-11-23 14:15:24 -0300

    poner setter aca si no no funciona

--- order-service/src/main/java/org/utn/ba/order/configuration/KafkaTopicProperties.java
+++ order-service/src/main/java/org/utn/ba/order/configuration/KafkaTopicProperties.java
@@ -1,12 +1,14 @@
 package org.utn.ba.order.configuration;
 
 import lombok.Getter;
+import lombok.Setter;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 @ConfigurationProperties(prefix = "kafka.topic")
 @Getter
+@Setter
 public class KafkaTopicProperties {
   private String OrderConfirmations;
   private String clearCart;
