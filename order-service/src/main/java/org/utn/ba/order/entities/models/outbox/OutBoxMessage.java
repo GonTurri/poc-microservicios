c95dec4b211f10c5162a41fef2e2ab2849d637e9
Gonzalo Ivo Turri <gturri@frba.utn.edu.ar>
2025-11-23 13:01:30 -0300

    create OutBox Message entity

---
+++ order-service/src/main/java/org/utn/ba/order/entities/models/outbox/OutBoxMessage.java
@@ -0,0 +1,39 @@
+package org.utn.ba.order.entities.models.outbox;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Lob;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import java.time.LocalDateTime;
+
+@Entity
+@Table
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Builder
+public class OutBoxMessage {
+  @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
+  private Long id;
+
+  @Column(name = "topic", nullable = false)
+  private String topic;
+
+  @Lob
+  @Column(name = "payload", nullable = false)
+  private String payload;
+
+  @Column(name = "creation_time", nullable = false)
+  private LocalDateTime creationTime;
+
+  @Column(name = "processed", nullable = false)
+  private Boolean processed = false;
+
+}
