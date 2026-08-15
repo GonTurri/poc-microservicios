9a2c06f609f3e6ff4c360a7adc5e6152f43384b8
Gonzalo Ivo Turri <gturri@frba.utn.edu.ar>
2026-08-15 14:16:00 -0300

    remove old file config

--- product-service/src/main/java/org/utn/ba/product/controllers/ProductController.java
+++ product-service/src/main/java/org/utn/ba/product/controllers/ProductController.java
@@ -6,10 +6,8 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
-import org.springframework.web.multipart.MultipartFile;
 import org.utn.ba.product.dto.ProductInputDTO;
 import org.utn.ba.product.dto.ProductOutputDTO;
-import org.utn.ba.product.services.IFileUploadService;
 import org.utn.ba.product.services.IProductService;
 
 import java.util.List;
--- product-service/src/main/java/org/utn/ba/product/services/IFileUploadService.java
+++
@@ -1,7 +0,0 @@
-package org.utn.ba.product.services;
-
-import org.springframework.web.multipart.MultipartFile;
-
-public interface IFileUploadService {
-  String saveImage(MultipartFile file);
-}
